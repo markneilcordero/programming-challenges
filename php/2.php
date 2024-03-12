8229db649178375e0170ff5b9c0d119042facc56
Mark Neil <mark.neil.u.cordero@gmail.com>
2024-03-12 22:33:33 +0800

    adding 2.php

---
+++ php/2.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+ * Write a program that will ask you your name 
+ * It will then display 'Hello Name' where 'Name' is the name you have entered.
+ * Example: What is your name?
+ * Ada
+ * Hello Ada
+ * 
+ * Prior knowledge needed
+ * How to create variables
+ * How to input data into a variable
+ * How to display variables
+ */
+
+$name = readline("What is your name?: ");
+echo "Hello $name\n";
+?>
